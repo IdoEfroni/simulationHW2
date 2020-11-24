1962e0488818c2b5fb93f930cd2901eab45cbcd3
IdoEfroni <idoefroni@gmail.com>
2020-11-24 17:43:37 +0200

    ido commit

--- form.py
+++ form.py
@@ -1,3 +1,8 @@
-
+import pedestrians
+import simulation
 class Equation:
     def __init__(self, num_of_pedestrians, room):
+        self.num_of_pedestrians = num_of_pedestrians
+        self.room = room
+        simula = simulation(num_of_pedestrians,room)
+        simula.place_pedestrians()
--- simulation.py
+++ simulation.py
@@ -1,27 +1,30 @@
 import numpy as np
 import pedestrians
+
+
 class Simulation:
-    def __init__(self, room, num_of_pedestrians):
+    def __init__(self, num_of_pedestrians, room):
         self.room = room
         self.num_of_pedestrians = num_of_pedestrians
         self.list_pedestrians = list()
 
+    def exist_place(self, place_x, place_y):
+        for p in self.list_pedestrians:
+            if abs(p.place[0] - place_x) < p.get_radii or abs(p.place[1] - place_y) < p.get_radii:
+                return True
+        return False
+
     def place_pedestrians(self):
         room_length = self.room.get_length()
         room_width = self.room.get_width()
-        place_list  =list();
+
         for i in range(self.num_of_pedestrians):
+            place_x = np.randint(0 + r, room_width - r)
+            place_y = np.randint(0 + r, room_length - r)
             r = np.random.uniform(0.5, 0.7) / 2
-            flag = True
-            while (flag):
-                place_x = np.randint(0 + r,room_width-r)
-                place_y = np.randint(0+ r, room_length-r)
-                flag2 =
-                for p in self.list_pedestrians:
-                    if abs(p.place[0]-place_x)<p.get_radii or abs(p.place[1]-place_y)<p.get_radii:
-                        continue
 
+            while self.exist_place(place_x, place_y):
+                place_x = np.randint(0 + r, room_width - r)
+                place_y = np.randint(0 + r, room_length - r)
 
-
-
-            self.list_pedestrians.append(pedestrians(1.5,,r),np.randint(60,100),0)
+            self.list_pedestrians.append(pedestrians(1.5, [place_x, place_y], r), np.randint(60, 90), 0)
